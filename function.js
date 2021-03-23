63f8865b2c054367cfcb81e055b68f902137a8e4
n-ce <abrocks1120@gmail.com>
2021-03-23 23:35:54 +0530

    1

---
+++ function.js
@@ -0,0 +1,1 @@
+function search() {let input = document.querySelector('input').value;input=input.toLowerCase();let x = document.querySelectorAll('li');for (i = 0; i < x.length; i++) {if (!x[i].innerHTML.toLowerCase().includes(input)) {x[i].style.display="none";}else {x[i].style.display="list-item";}}}
