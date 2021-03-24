24b019bd4244b68c525b44b34833ba97c9eaf170
n-ce <abrocks1120@gmail.com>
2021-03-24 10:47:49 +0530

    dark mode

--- function.js
+++ function.js
@@ -1,1 +1,8 @@
 function search() {let input = document.querySelector('input').value;input=input.toLowerCase();let x = document.querySelectorAll('li');for (i = 0; i < x.length; i++) {if (!x[i].innerHTML.toLowerCase().includes(input)) {x[i].style.display="none";}else {x[i].style.display="list-item";}}}
+function darkmode()
+{
+	if (document.querySelector("input").checked == true) {
+		document.body.style.backgroundColor = "black";
+	} else {
+		document.body.style.backgroundColor = "white";
+	}}
