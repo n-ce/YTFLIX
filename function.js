f2bd626220373844f46f9756fc90ac696940c0ea
n-ce <abrocks1120@gmail.com>
2021-08-29 20:03:28 +0530

    v1.6 : Earthquake - New Animations, Navigation Interface and Grid Interface

--- function.js
+++ function.js
@@ -7,10 +7,13 @@ var checkBox = document.querySelector("input");
 	
 	checkBox.addEventListener('change', function() {
 		if (this.checked) {
-			document.body.style.backgroundColor = "black";
+			document.body.style.backgroundColor = document.body.querySelector('.nav').style.backgroundColor = "black";
+			document.querySelector("meta[name=theme-color]").setAttribute("content","black");
 			window.localStorage.setItem('data-theme', 'dark');
 		} else {
 			document.body.style.backgroundColor = "white";
+			document.body.querySelector('.nav').style.backgroundColor = "#333333";
+			document.querySelector("meta[name=theme-color]").setAttribute("content","#333333");
 			window.localStorage.setItem('data-theme', 'light');
 		}
 	});
