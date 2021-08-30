71fc4b562abd3d98653ff754dd070fd90a55f1c7
n-ce <abrocks1120@gmail.com>
2021-08-30 23:43:31 +0530

    small fixes

--- Scripts/DarkMode.js
+++ Scripts/DarkMode.js
@@ -3,22 +3,19 @@ var theme = window.localStorage.getItem('data-theme');
 var body = document.body.style;
 var nav = document.querySelector('.nav').style;
 function meta(y) {
-  return document.querySelector('meta[name="theme-color"]').setAttribute("content", y);
+  return document.querySelector('meta[name="theme-color"]').setAttribute("content", y),body.backgroundColor = nav.backgroundColor = y;
 }
 if (theme == 'dark'){
-  body.backgroundColor = nav.backgroundColor = "black";
   meta("black");
   checkBox.checked = theme == 'dark' ? true : false;
 	}
 checkBox.onchange=function() {
   if (checkBox.checked==true) {
-    body.backgroundColor = nav.backgroundColor = "black";
     meta("black");
     window.localStorage.setItem('data-theme', 'dark');
   }else{
-    body.backgroundColor = "white";
-		nav.backgroundColor = "#333333";
 		meta("#333333");
+		body.backgroundColor = "white";
 		window.localStorage.setItem('data-theme', 'light');
   }
 }
