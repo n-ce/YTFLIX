899789c21b520ec0dda80511b8dff65bbc19267c
Enceladus <69345507+n-ce@users.noreply.github.com>
2021-08-31 08:39:23 +0530

    Fix attempt 2

--- Scripts/DarkMode.js
+++ Scripts/DarkMode.js
@@ -3,19 +3,22 @@ var theme = window.localStorage.getItem('data-theme');
 var body = document.body.style;
 var nav = document.querySelector('.nav').style;
 function meta(y) {
-  return document.querySelector('meta[name="theme-color"]').setAttribute("content", y),body.backgroundColor = nav.backgroundColor = y;
+  return document.querySelector('meta[name="theme-color"]').setAttribute("content", y);
 }
-if (theme == 'dark'){
-  meta("black");}
+if (theme == 'dark')
+  meta("black");
+  body.backgroundColor = nav.backgroundColor = "black";
   checkBox.checked = theme == 'dark' ? true : false;
 	
 checkBox.onchange=function() {
   if (checkBox.checked==true) {
     meta("black");
+    body.backgroundColor = nav.backgroundColor = "black";
     window.localStorage.setItem('data-theme', 'dark');
   }else{
-		meta("#333333");
-		body.backgroundColor = "white";
-		window.localStorage.setItem('data-theme', 'light');
+    meta("#333333");
+    nav.backgroundColor = "#333333";
+    body.backgroundColor = "white";
+    window.localStorage.setItem('data-theme', 'light');
   }
 }
