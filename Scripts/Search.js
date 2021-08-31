c10b8cf4b375e93a0dd9e1b8e57a5a0939caa26a
Enceladus <69345507+n-ce@users.noreply.github.com>
2021-08-31 08:50:11 +0530

    Fix attempt 2

--- Scripts/Search.js
+++ Scripts/Search.js
@@ -1,4 +1,4 @@
-const search = document.querySelector('input');
+const search = document.querySelector("input");
 search.onkeyup=function(){
   let input = search.value;
   input=input.toLowerCase();
