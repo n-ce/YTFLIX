d011db82b0edd9fe37f47cd0a783adabc8c7c19b
n-ce <abrocks1120@gmail.com>
2021-09-02 23:14:18 +0530

    PWA TEST 6

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ self.addEventListener("fetch", fetchEvent => {
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", function() {
     navigator.serviceWorker
-      .register("Script.js")
+      .register("script.js")
       .then(res => console.log("service worker registered"))
       .catch(err => console.log("service worker not registered", err))
   })
