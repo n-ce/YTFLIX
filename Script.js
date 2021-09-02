f73ab445ab1434ae51d4849d6e2756dfca0aec7b
n-ce <abrocks1120@gmail.com>
2021-09-02 22:57:39 +0530

    PWA TEST 2

--- Script.js
+++ Script.js
@@ -1,3 +1,36 @@
+//Progressive Web App
+const staticLoader = "YTFLIX"
+const assets = [
+  "/",
+  "index.html",
+  "Assets/Icons/maskable_icon_x512.webp",
+  "Assets/Icons/maskable_icon_x512.webp"
+]
+
+self.addEventListener("install", installEvent => {
+  installEvent.waitUntil(
+    caches.open(staticLoader).then(cache => {
+      cache.addAll(assets)
+    })
+  )
+})
+self.addEventListener("fetch", fetchEvent => {
+  fetchEvent.respondWith(
+    caches.match(fetchEvent.request).then(res => {
+      return res || fetch(fetchEvent.request)
+    })
+  )
+})
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", function() {
+    navigator.serviceWorker
+      .register("service-worker.js")
+      .then(res => console.log("service worker registered"))
+      .catch(err => console.log("service worker not registered", err))
+  })
+}
+
 //DARK MODE
 var checkBox = document.querySelector('.uk-checkbox');
 var theme = window.localStorage.getItem('data-theme');
@@ -89,35 +122,3 @@ function appendData(data) {
   }
 }
 
-//Progressive Web App
-const staticLoader = "YTFLIX"
-const assets = [
-  "/",
-  "index.html",
-  "Assets/Icons/maskable_icon_x512.webp",
-  "Assets/Icons/maskable_icon_x512.webp"
-]
-
-self.addEventListener("install", installEvent => {
-  installEvent.waitUntil(
-    caches.open(staticLoader).then(cache => {
-      cache.addAll(assets)
-    })
-  )
-})
-self.addEventListener("fetch", fetchEvent => {
-  fetchEvent.respondWith(
-    caches.match(fetchEvent.request).then(res => {
-      return res || fetch(fetchEvent.request)
-    })
-  )
-})
-
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", function() {
-    navigator.serviceWorker
-      .register("service-worker.js")
-      .then(res => console.log("service worker registered"))
-      .catch(err => console.log("service worker not registered", err))
-  })
-}
