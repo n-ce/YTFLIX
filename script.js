c63bfffa1cd6e1e9ec022f474b4be676d4f3a0e0
n-ce <abrocks1120@gmail.com>
2021-09-03 10:06:08 +0530

    v2.1 : PWA, crimson nav color back, and CSS optimization

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 //DARK MODE
 var checkBox = document.querySelector('.uk-checkbox');
-var theme = window.localStorage.getItem('data-theme');
 var body = document.body.style;
 var nav = document.querySelector('nav').style;
 function meta(y) {
   return document.querySelector('meta[name="theme-color"]').setAttribute("content", y),body.backgroundColor = nav.backgroundColor = y,window.localStorage.setItem('data-theme', y);
 }
-if (theme == "black"){
+if (window.localStorage.getItem('data-theme') == "black"){
   meta("black");
   checkBox.checked = true;
 }
@@ -14,7 +13,7 @@ checkBox.onclick=function() {
   if (checkBox.checked==true) {
     meta("black");
   }else{
-    meta("#333333");
+    meta("crimson");
     body.backgroundColor="white";
   }
 }
