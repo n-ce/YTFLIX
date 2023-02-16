fc9cd840d474a43028716628f7bacb32d61dca0e
abrocks1120@gmail.com <abrocks1120@gmail.com>
2023-02-16 19:10:12 +0530

    wikified the categories and related metadata

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //DARK MODE
 const checkBox = document.querySelector('.uk-checkbox');
 
-function theme(navColor, bodyColor) {
+const theme = (navColor, bodyColor) => {
   document.querySelector('meta[name="theme-color"]').setAttribute("content", navColor);
   document.querySelector('nav').style.backgroundColor = navColor;
   document.body.style.backgroundColor = bodyColor;
@@ -28,7 +28,7 @@ if (localStorage.getItem('data-theme')) {
   theme("#1f1f1f", "#121212");
   checkBox.checked = true;
 }
-checkBox.onclick = function() {
+checkBox.addEventListener('click', () => {
   if (checkBox.checked) {
     localStorage.setItem('data-theme', "#1f1f1f");
     theme("#1f1f1f", "#121212");
@@ -36,41 +36,45 @@ checkBox.onclick = function() {
     localStorage.clear();
     theme("#ff4548", "white");
   }
-}
+});
 
 //SEARCH
-
-document.querySelector(".uk-search-input").onkeyup = function() {
+const search = document.getElementsByClassName("uk-search-input")[0];
+search.addEventListener('keyup', () => {
   for (const x of document.querySelectorAll('li'))
-    !x.innerHTML.toLowerCase().includes(this.value.toLowerCase()) ?
+    !x.innerHTML.toLowerCase().includes(search.value.toLowerCase()) ?
     x.style.display = "none" :
     x.style.display = "list-item";
-}
+});
+
+
 
 //FETCH DATA
-const addlist = document.querySelectorAll('.uk-list');
-const categories = ['Originals', 'Anime', 'English', 'Hindi', 'Korean'];
-const categoryHidden = [true, true, true, true, true];
+let items;
+fetch('https://raw.githubusercontent.com/wiki/n-ce/YTFLIX/Home.md')
+  .then(res => res.text())
+  .then(homeMD => JSON.parse(homeMD.slice(3)))
+  .then(homeJSON => {
+    for (const category of homeJSON) {
 
-categories.forEach((v, i) => {
+      fetch(`https://raw.githubusercontent.com/wiki/n-ce/YTFLIX/${category.name}.md`)
+        .then(res => res.text())
+        .then(categoryMD => JSON.parse(categoryMD.slice(3)))
+        .then(categoryJSON => {
+          let list = '';
+          for (const object of categoryJSON)
+            list += `<li><a href="https://youtube.com/${object.URL}">${object.Name}</a></li>`;
 
-  fetch(`https://raw.githubusercontent.com/wiki/n-ce/YTFLIX/${v}.md`)
-    .then(res => res.text())
-    .then(text => { // ⬇️ converts markdown to json
-      for (const x of JSON.parse(text.slice(3))) {
-        const list = document.createElement("li");
-        list.innerHTML = `<a href="https://youtube.com/${x.URL}">${x.Name}</a>`;
-        addlist[i].appendChild(list);
-      }
-    })
-    .catch(err => alert(err));
-
-  addlist[i].style.display = 'none';
-
-  document.querySelectorAll('li')[i].onclick = function() {
-    categoryHidden[i] ?
-      addlist[i].style.display = 'block' :
-      addlist[i].style.display = 'none';
-    categoryHidden[i] = !categoryHidden[i];
-  }
-})
+          document.getElementById('container').innerHTML +=
+            `<li onclick="document.getElementById('${category.name}').classList.toggle('hide')">
+          <a class="uk-accordion-title uk-card uk-card-hover uk-card-body uk-background-cover uk-card-secondary" data-src=${category.thumbnail} uk-img>
+            <h3 class="uk-card-title uk-text-bolder">${category.name}</h3>
+            <p id="text">${category.description}</p>
+          </a>
+          <ul id="${category.name}" class="uk-accordion-content uk-list uk-list-large uk-list-divider hide">${list}</ul>
+        </li>`;
+        })
+        .catch(err => console.log(err));
+    }
+  })
+  .catch(err => console.log('e:' + err))
