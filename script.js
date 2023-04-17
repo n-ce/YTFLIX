ed581faf7a4d8d86b5f93e98c81b566b3047610d
abrocks1120@gmail.com <abrocks1120@gmail.com>
2023-04-17 23:16:15 +0530

    removed pwa support, use top level await to fetch home data, use create element instead of innerhtml, search now shows full list

--- script.js
+++ script.js
@@ -1,23 +1,28 @@
+// HOMEPAGE DATA FETCHING IN ADVANCE USING TOP LEVEL AWAIT
+const homeData = await fetch('https://raw.githubusercontent.com/wiki/n-ce/YTFLIX/Home.md')
+	.then(res => res.text())
+	.then(homeMD => JSON.parse(homeMD.slice(3)))
+
 //DARK MODE
 const checkBox = document.querySelector('.uk-checkbox');
 
 const theme = (navColor, bodyColor) => {
-  document.querySelector('meta[name="theme-color"]').setAttribute("content", navColor);
-  document.querySelector('nav').style.backgroundColor = navColor;
-  document.body.style.backgroundColor = bodyColor;
+	document.querySelector('meta[name="theme-color"]').setAttribute("content", navColor);
+	document.querySelector('nav').style.backgroundColor = navColor;
+	document.body.style.backgroundColor = bodyColor;
 }
 
 //prefers color scheme
 let colorSchemeQueryList = matchMedia('(prefers-color-scheme: dark)');
 
 const setColorScheme = e => {
-  if (e.matches) {
-    theme("#1f1f1f", "#121212");
-    checkBox.checked = true;
-  } else {
-    theme("#ff4548", "white");
-    checkBox.checked = false;
-  }
+	if (e.matches) {
+		theme("#1f1f1f", "#121212");
+		checkBox.checked = true;
+	} else {
+		theme("#ff4548", "white");
+		checkBox.checked = false;
+	}
 }
 
 setColorScheme(colorSchemeQueryList);
@@ -25,56 +30,86 @@ colorSchemeQueryList.addListener(setColorScheme);
 
 //local storage
 if (localStorage.getItem('data-theme')) {
-  theme("#1f1f1f", "#121212");
-  checkBox.checked = true;
+	theme("#1f1f1f", "#121212");
+	checkBox.checked = true;
 }
 checkBox.addEventListener('click', () => {
-  if (checkBox.checked) {
-    localStorage.setItem('data-theme', "#1f1f1f");
-    theme("#1f1f1f", "#121212");
-  } else {
-    localStorage.clear();
-    theme("#ff4548", "white");
-  }
+	if (checkBox.checked) {
+		localStorage.setItem('data-theme', "#1f1f1f");
+		theme("#1f1f1f", "#121212");
+	} else {
+		localStorage.clear();
+		theme("#ff4548", "white");
+	}
 });
 
 //SEARCH
 const search = document.getElementsByClassName("uk-search-input")[0];
+const categories = document.getElementsByClassName('uk-card');
+document.getElementById('search').onclick = () => {
+	for (const i of categories) {
+		if (i.nextElementSibling.classList.contains('hide'))
+				i.click();
+			i.classList.add('hide');
+	}
+}
+document.getElementsByClassName('uk-navbar-toggle')[0].onclick = () => {
+	for (const i of categories) {
+		i.click();
+		i.classList.remove('hide')
+	}
+}
 search.addEventListener('keyup', () => {
-  for (const x of document.querySelectorAll('li'))
-    !x.innerHTML.toLowerCase().includes(search.value.toLowerCase()) ?
-    x.style.display = "none" :
-    x.style.display = "list-item";
+	const list = document.querySelectorAll('li');
+	for (const x of list)
+		x.style.display = !x.textContent.toLowerCase().includes(search.value.toLowerCase()) ?
+		"none" :
+		"list-item";
 });
 
 
 
-//FETCH DATA
-let items;
-fetch('https://raw.githubusercontent.com/wiki/n-ce/YTFLIX/Home.md')
-  .then(res => res.text())
-  .then(homeMD => JSON.parse(homeMD.slice(3)))
-  .then(homeJSON => {
-    for (const category of homeJSON) {
+// DATA INJECTION
 
-      fetch(`https://raw.githubusercontent.com/wiki/n-ce/YTFLIX/${category.name}.md`)
-        .then(res => res.text())
-        .then(categoryMD => JSON.parse(categoryMD.slice(3)))
-        .then(categoryJSON => {
-          let list = '';
-          for (const object of categoryJSON)
-            list += `<li><a href="https://youtube.com/${object.URL}">${object.Name}</a></li>`;
 
-          document.getElementById('container').innerHTML +=
-            `<li onclick="document.getElementById('${category.name}').classList.toggle('hide')">
-          <a class="uk-accordion-title uk-card uk-card-hover uk-card-body uk-background-cover uk-card-secondary" data-src=${category.thumbnail} uk-img>
-            <h3 class="uk-card-title uk-text-bolder">${category.name}</h3>
-            <p id="text">${category.description}</p>
-          </a>
-          <ul id="${category.name}" class="uk-accordion-content uk-list uk-list-large uk-list-divider hide">${list}</ul>
-        </li>`;
-        })
-        .catch(err => console.log(err));
-    }
-  })
-  .catch(err => console.log('e:' + err))
+for (const category of homeData) {
+
+	fetch(`https://raw.githubusercontent.com/wiki/n-ce/YTFLIX/${category.name}.md`)
+		.then(res => res.text())
+		.then(categoryMD => JSON.parse(categoryMD.slice(3)))
+		.then(categoryJSON => {
+
+			const h3 = document.createElement('h3');
+			h3.classList.add("uk-card-title", "uk-text-bolder");
+			h3.textContent = category.name;
+
+			const p = document.createElement('p');
+			p.id = 'text';
+			p.textContent = category.description;
+
+			const a = document.createElement('a');
+			a.classList.add("uk-accordion-title", "uk-card", "uk-card-hover", "uk-card-body", "uk-background-cover", "uk-card-secondary");
+			a.dataset.src = category.thumbnail;
+			a.setAttribute('uk-img', '');
+			a.append(h3, p);
+
+			const ul = document.createElement('ul');
+			ul.id = category.name;
+			ul.classList.add("uk-accordion-content", "uk-list", "uk-list-large", "uk-list-divider", "hide")
+			for (const object of categoryJSON) {
+				const list = document.createElement('li');
+				const anchor = document.createElement('a');
+				anchor.href = "https://youtube.com/" + object.URL;
+				anchor.textContent = object.Name;
+				list.appendChild(anchor);
+				ul.appendChild(list);
+			}
+
+			const li = document.createElement('li');
+			li.append(a, ul);
+			li.setAttribute('onclick', 'this.lastElementChild.classList.toggle("hide")');
+
+			document.getElementById('container').appendChild(li);
+		})
+		.catch(err => console.log(err));
+}
