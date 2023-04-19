e5a86404f0369ae0f1afd4727dc67555410e401e
abrocks1120@gmail.com <abrocks1120@gmail.com>
2023-04-19 17:31:15 +0530

    removed netlify form integration

--- script.js
+++ script.js
@@ -1,14 +1,9 @@
-// HOMEPAGE DATA FETCHING IN ADVANCE USING TOP LEVEL AWAIT
-const homeData = await fetch('https://raw.githubusercontent.com/wiki/n-ce/YTFLIX/Home.md')
-	.then(res => res.text())
-	.then(homeMD => JSON.parse(homeMD.slice(3)))
-
 //DARK MODE
-const checkBox = document.querySelector('.uk-checkbox');
+const checkBox = document.getElementById('checkbox');
 
-const theme = (navColor, bodyColor) => {
-	document.querySelector('meta[name="theme-color"]').setAttribute("content", navColor);
-	document.querySelector('nav').style.backgroundColor = navColor;
+function theme(navColor, bodyColor) {
+	document.getElementById('tabColor').setAttribute("content", navColor);
+	document.getElementById('nav').style.backgroundColor = navColor;
 	document.body.style.backgroundColor = bodyColor;
 }
 
@@ -44,24 +39,27 @@ checkBox.addEventListener('click', () => {
 });
 
 //SEARCH
-const search = document.getElementsByClassName("uk-search-input")[0];
+const search = document.getElementById("search_input");
 const categories = document.getElementsByClassName('uk-card');
-document.getElementById('search').onclick = () => {
+const search_list = document.getElementsByTagName('li');
+
+document.getElementById('search_button').onclick = () => {
 	for (const i of categories) {
 		if (i.nextElementSibling.classList.contains('hide'))
-				i.click();
-			i.classList.add('hide');
+			i.click();
+		i.classList.add('hide');
 	}
 }
 document.getElementsByClassName('uk-navbar-toggle')[0].onclick = () => {
+	search.value = '';
+	for (const li of search_list) li.style.display = 'list-item';
 	for (const i of categories) {
 		i.click();
-		i.classList.remove('hide')
+		i.classList.remove('hide');
 	}
 }
 search.addEventListener('keyup', () => {
-	const list = document.querySelectorAll('li');
-	for (const x of list)
+	for (const x of search_list)
 		x.style.display = !x.textContent.toLowerCase().includes(search.value.toLowerCase()) ?
 		"none" :
 		"list-item";
@@ -72,44 +70,48 @@ search.addEventListener('keyup', () => {
 // DATA INJECTION
 
 
-for (const category of homeData) {
+fetch('https://raw.githubusercontent.com/wiki/n-ce/YTFLIX/Home.md')
+	.then(res => res.text())
+	.then(homeMD => JSON.parse(homeMD.slice(3)))
+	.then(homeData => {
+		for (const category of homeData) {
 
-	fetch(`https://raw.githubusercontent.com/wiki/n-ce/YTFLIX/${category.name}.md`)
-		.then(res => res.text())
-		.then(categoryMD => JSON.parse(categoryMD.slice(3)))
-		.then(categoryJSON => {
+			fetch(`https://raw.githubusercontent.com/wiki/n-ce/YTFLIX/${category.name}.md`)
+				.then(res => res.text())
+				.then(categoryMD => JSON.parse(categoryMD.slice(3)))
+				.then(categoryJSON => {
 
-			const h3 = document.createElement('h3');
-			h3.classList.add("uk-card-title", "uk-text-bolder");
-			h3.textContent = category.name;
+					const h3 = document.createElement('h3');
+					h3.classList.add("uk-card-title", "uk-text-bolder");
+					h3.textContent = category.name;
 
-			const p = document.createElement('p');
-			p.id = 'text';
-			p.textContent = category.description;
+					const p = document.createElement('p');
+					p.id = 'text';
+					p.textContent = category.description;
 
-			const a = document.createElement('a');
-			a.classList.add("uk-accordion-title", "uk-card", "uk-card-hover", "uk-card-body", "uk-background-cover", "uk-card-secondary");
-			a.dataset.src = category.thumbnail;
-			a.setAttribute('uk-img', '');
-			a.append(h3, p);
+					const a = document.createElement('a');
+					a.classList.add("uk-accordion-title", "uk-card", "uk-card-hover", "uk-card-body", "uk-background-cover", "uk-card-secondary");
+					a.dataset.src = category.thumbnail;
+					a.setAttribute('uk-img', '');
+					a.append(h3, p);
 
-			const ul = document.createElement('ul');
-			ul.id = category.name;
-			ul.classList.add("uk-accordion-content", "uk-list", "uk-list-large", "uk-list-divider", "hide")
-			for (const object of categoryJSON) {
-				const list = document.createElement('li');
-				const anchor = document.createElement('a');
-				anchor.href = "https://youtube.com/" + object.URL;
-				anchor.textContent = object.Name;
-				list.appendChild(anchor);
-				ul.appendChild(list);
-			}
+					const ul = document.createElement('ul');
+					ul.id = category.name;
+					ul.classList.add("uk-accordion-content", "uk-list", "uk-list-large", "uk-list-divider", "hide")
+					for (const object of categoryJSON) {
+						const list = document.createElement('li');
+						const anchor = document.createElement('a');
+						anchor.href = "https://youtube.com/" + object.URL;
+						anchor.textContent = object.Name;
+						list.appendChild(anchor);
+						ul.appendChild(list);
+					}
 
-			const li = document.createElement('li');
-			li.append(a, ul);
-			li.setAttribute('onclick', 'this.lastElementChild.classList.toggle("hide")');
+					const li = document.createElement('li');
+					li.append(a, ul);
+					li.setAttribute('onclick', `document.getElementById('${category.name}').classList.toggle("hide")`);
 
-			document.getElementById('container').appendChild(li);
-		})
-		.catch(err => console.log(err));
-}
+					document.getElementById('container').appendChild(li);
+				})
+		}
+	});
